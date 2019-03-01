0c0c58fe6adec82cd7905d4eed9a9dffab5ca8a9
Masato Ikeda <masato.ikeda@gmail.com>
2019-03-01 22:35:04 +0900

    Initial commit

---
+++ lib/capistrano/middleman2.rb
@@ -0,0 +1,7 @@
+require "capistrano/middleman2/version"
+
+module Capistrano
+  module Middleman2
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/middleman2/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Middleman2
+    VERSION = "0.1.0"
+  end
+end
